76fdb1ac88d9eb90cb4d43c3d0e177a6c69d717c
SAPIENT\vsha95 <vsharma109@sapient.com>
2018-11-28 16:22:41 +0530

    added config

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongodburi: 'mongodb://localhost:27017/buynsell',
+  DBURL: 'mongodb://vishnuksharma:vishnu123@ds161931.mlab.com:61931/buynsell',
+  auth: {
+    secret: '',
+  },
+};
