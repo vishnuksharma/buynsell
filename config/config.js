f7dddacb7931b3ed5070f7384c1263d0b8aea915
Vishnu Sharma <vish.sharma93@gmail.com>
2021-01-30 16:46:03 +0800

    Update config.js
    updated mongodb url

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodburi: 'mongodb://localhost:27017/buynsell',
-  DBURL: 'mongodb://vishnuksharma:vishnu123@ds161931.mlab.com:61931/buynsell',
+  DBURL: 'mongodb+srv://mongodb:mongodb@cluster0.fnsnt.mongodb.net/buynsell?retryWrites=true&w=majority'
   auth: {
     secret: '',
   },
