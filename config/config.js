3bc7f34925e2b694e6ca8fd68bf96aa0a259fe86
Vishnu Sharma <vish.sharma@gmail.com>
2021-01-31 12:03:03 +0800

    change db config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodburi: 'mongodb://localhost:27017/buynsell',
-  DBURL: 'mongodb+srv://mongodb:mongodb@cluster0.fnsnt.mongodb.net/buynsell?retryWrites=true&w=majority'
+  DBURL: 'mongodb+srv://mongodb:mongodb@cluster0.fnsnt.mongodb.net/buynsell?retryWrites=true&w=majority',
   auth: {
     secret: '',
   },
